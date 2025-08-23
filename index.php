2020d5ad940b6a22b3aa11263c049a8429790f1f
asroful7 <37543160+asroful7@users.noreply.github.com>
2025-08-23 11:31:54 +0700

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-Situs nonton Anime Donghua Movie Drama subtitle indonesia
-Kini kamu gak perlu repot repot mencari situs buat hiburan streaming anime,donghua,moviedan drama subtitle indonesia
-Setiap orang mungkin memilih situs yang nyaman dan tanpa iklan
-kini tersedia di https://hibstream.blogspot.com
-kalian bisa menonton dan mendownload tanpa iklan ,Serta di web hibstream sendiri sudah memiliki lengkap fiturnya .buruan kunjungi tunggu apa lagi😎
